c28745952a566e00a24d45d4c0daa99cf264dd46
gagu00000 <gagu00000@gmail.com>
2026-01-12 23:20:14 +0400

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,9 +1,5 @@
-# ============================================================================
-# UAE Pulse Simulator + Data Rescue Dashboard
-# Modules Package
-# ============================================================================
-
 from .cleaner import DataCleaner
+from .simulator import Simulator
 from .utils import *
 
-__all__ = ['DataCleaner']
+__all__ = ['DataCleaner', 'Simulator']
